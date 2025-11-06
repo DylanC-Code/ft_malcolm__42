00c1bc33da8ad9f3b6eaa643fdd21c61fdeb0b18
DylanC-Code <dylanc.code@gmail.com>
2025-11-06 14:06:44 +0100

    refactor: update header and source files to remove unused config struct and adjust includes

--- includes/ft_malcolm.h
+++ includes/ft_malcolm.h
@@ -6,17 +6,19 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 18:42:05 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/06 12:16:56 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/06 14:03:55 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MALCOM_H
 #define FT_MALCOM_H
 
-typedef struct config t_config;
+// typedef struct config t_config;
 
 #include <stdlib.h>
 
+#include "domain/mac.h"
+// #include "domain/config.h"
 #include "infrastructure/shared.h"
 #include "infrastructure/cli/cli_config_parser.h"
 #include "infrastructure/net/mac_parser.h"
--- srcs/ft_malcolm.c
+++ srcs/ft_malcolm.c
@@ -6,7 +6,7 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 18:42:08 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/06 11:33:37 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/06 14:05:32 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 int main(int argc, char **argv)
 {
     t_config config;
-    // t_config *config = parse_cli_config(argc, argv);
     if (parse_cli_config(&config, argc, argv) == EXIT_FAILURE)
         return 1;
-
+    // print_mac(&config.src_mac);
+    // __builtin_printf("\n");
+    // print_mac(&config.tgt_mac);
+    // __builtin_printf("\n");
     // destroy_config(config);
     return 0;
 }
