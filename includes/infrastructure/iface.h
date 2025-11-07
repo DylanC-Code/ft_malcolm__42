208b9a72a82afb69b7badc156ca6d9d715e37e59
DylanC-Code <dylanc.code@gmail.com>
2025-11-07 12:36:32 +0100

    feat: enhance iface module with additional status codes and recv function

--- includes/infrastructure/iface.h
+++ includes/infrastructure/iface.h
@@ -6,7 +6,7 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 21:55:07 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/06 23:06:56 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/07 12:28:30 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,21 @@ typedef enum e_iface_status
 {
     IFACE_CLOSING_SUCCESS,
     IFACE_OPENING_SUCCESS,
+    IFACE_RECV_SUCCESS,
     IFACE_ALLOC_FAIL,
     IFACE_SOCKET_FAIL,
     IFACE_IFINDEX_FAIL,
     IFACE_BIND_FAIL,
+    IFACE_RECV_FAIL,
+    IFACE_CONTENT_TOO_SHORT,
+    IFACE_INVALID_PROTOCOL,
+    IFACE_INVALID_ARP_OPERATION,
 } t_iface_status;
 
 char *iface_strerror(t_iface_status err);
 
 t_iface_status iface_open(t_iface **p_iface);
 t_iface_status iface_close(t_iface *iface);
+t_iface_status iface_recv(t_iface *iface, unsigned char *dst, size_t dst_size, ssize_t *bytes_read);
 
 #endif
--- srcs/infrastructure/iface.c
+++ srcs/infrastructure/iface.c
@@ -6,15 +6,19 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 21:54:53 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/06 23:08:23 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/07 12:27:33 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <linux/if_ether.h>
-#include <net/if.h>
 #include <linux/if_packet.h>
 #include <net/ethernet.h>
+#include <net/if_arp.h>
+// #include <netinet/if_ether.h>
+#include <net/if.h>
 #include <unistd.h>
+#include <stddef.h>
+#include <stdio.h>
 
 #include "infrastructure/iface.h"
 #include "infrastructure/shared.h"
@@ -22,59 +26,101 @@
 
 typedef struct s_iface
 {
-    int fd;
-    unsigned if_index;
-    struct sockaddr_ll addr;
+	int fd;
+	unsigned if_index;
+	struct sockaddr_ll addr;
 } t_iface;
 
 char *iface_strerror(t_iface_status err)
 {
-    if (err == IFACE_OPENING_SUCCESS)
-        return "interface opened successfully";
-    else if (err == IFACE_CLOSING_SUCCESS)
-        return "interface closed successfully";
-    else if (err == IFACE_ALLOC_FAIL)
-        return "iface memory allocation failed";
-    else if (err == IFACE_SOCKET_FAIL)
-        return "opening socket failed";
-    else if (err == IFACE_IFINDEX_FAIL)
-        return "retrieving interface index failed";
-    else if (err == IFACE_BIND_FAIL)
-        return "binding socket to interface failed";
-    else
-        return "unknown iface error";
+	if (err == IFACE_OPENING_SUCCESS)
+		return "interface opened successfully";
+	else if (err == IFACE_CLOSING_SUCCESS)
+		return "interface closed successfully";
+	else if (err == IFACE_RECV_SUCCESS)
+		return "data read successfully from interface";
+	else if (err == IFACE_ALLOC_FAIL)
+		return "iface memory allocation failed";
+	else if (err == IFACE_SOCKET_FAIL)
+		return "opening socket failed";
+	else if (err == IFACE_IFINDEX_FAIL)
+		return "retrieving interface index failed";
+	else if (err == IFACE_BIND_FAIL)
+		return "binding socket to interface failed";
+	else if (err == IFACE_RECV_FAIL)
+		return "receiving data from interface failed";
+	else if (err == IFACE_CONTENT_TOO_SHORT)
+		return "received content is too short";
+	else if (err == IFACE_INVALID_PROTOCOL)
+		return "received content has invalid protocol";
+	else if (err == IFACE_INVALID_ARP_OPERATION)
+		return "received content has invalid ARP operation";
+	else
+		return "unknown iface error";
 }
 
 t_iface_status iface_open(t_iface **p_iface)
 {
-    t_iface *iface = ft_calloc(1, sizeof(t_iface));
-    if (!iface)
-        return IFACE_ALLOC_FAIL;
-    iface->fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
-    if (iface->fd == -1)
-        return IFACE_SOCKET_FAIL;
-    iface->if_index = if_nametoindex("eth0");
-    if (!iface->if_index)
-        return IFACE_IFINDEX_FAIL;
-    iface->addr.sll_family = AF_PACKET;
-    iface->addr.sll_protocol = htons(ETH_P_ARP);
-    iface->addr.sll_ifindex = iface->if_index;
-    iface->addr.sll_halen = ETH_ALEN;
-    if (bind(iface->fd, (const struct sockaddr *)&iface->addr, sizeof(struct sockaddr_ll)) != 0)
-    {
-        iface_close(iface);
-        return IFACE_BIND_FAIL;
-    }
-    *p_iface = iface;
-    return IFACE_OPENING_SUCCESS;
+	t_iface *iface = ft_calloc(1, sizeof(t_iface));
+
+	if (!iface)
+		return IFACE_ALLOC_FAIL;
+	iface->fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
+	if (iface->fd == -1)
+		return IFACE_SOCKET_FAIL;
+	iface->if_index = if_nametoindex("eth0");
+	if (!iface->if_index)
+		return IFACE_IFINDEX_FAIL;
+	iface->addr.sll_family = AF_PACKET;
+	iface->addr.sll_protocol = htons(ETH_P_ARP);
+	iface->addr.sll_ifindex = iface->if_index;
+	iface->addr.sll_halen = ETH_ALEN;
+	if (bind(iface->fd, (const struct sockaddr *)&iface->addr, sizeof(struct sockaddr_ll)) != 0)
+	{
+		iface_close(iface);
+		return IFACE_BIND_FAIL;
+	}
+	*p_iface = iface;
+	return IFACE_OPENING_SUCCESS;
 }
 
 t_iface_status iface_close(t_iface *iface)
 {
-    if (!iface)
-        return false;
-    if (iface->fd > 2)
-        close(iface->fd);
-    free(iface);
-    return IFACE_CLOSING_SUCCESS;
+	if (iface->fd > 2)
+		close(iface->fd);
+	free(iface);
+	return IFACE_CLOSING_SUCCESS;
+}
+
+static inline uint16_t get_u16(const void *ptr)
+{
+	uint16_t res = 0;
+	ft_memcpy(&res, ptr, sizeof(uint16_t));
+	return htons(res);
+}
+
+t_iface_status iface_recv(t_iface *iface, unsigned char *dst, size_t dst_size, ssize_t *bytes_read)
+{
+	// unsigned char buff[65536];
+	// struct sockaddr_ll src;
+	// socklen_t slen = sizeof(src);
+
+	*bytes_read = recvfrom(iface->fd, dst, dst_size, 0, NULL, NULL);
+
+	if (*bytes_read == -1)
+		return IFACE_RECV_FAIL;
+	if (*bytes_read < ETH_HLEN)
+		return IFACE_CONTENT_TOO_SHORT;
+	return IFACE_RECV_SUCCESS;
+	// if (bytes_read < ETH_HLEN + 28)
+	// 	return IFACE_CONTENT_TOO_SHORT;
+
+	// uint16_t ethertype = get_u16(buff + offsetof(struct ethhdr, h_proto));
+	// uint16_t arp_op = get_u16(buff + ETH_HLEN + offsetof(struct arphdr, ar_op));
+
+	// if (ethertype != ETH_P_ARP)
+	// 	return IFACE_INVALID_PROTOCOL;
+	// if (arp_op == ARPOP_REQUEST)
+	// 	__builtin_printf("ARP request reçue, taille=%zd\n", bytes_read);
+	// return IFACE_INVALID_ARP_OPERATION;
 }
