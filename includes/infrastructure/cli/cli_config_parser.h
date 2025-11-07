f5d712870563ea15b64d0baf5bd64e91f5ab8fe8
DylanC-Code <dylanc.code@gmail.com>
2025-11-07 17:18:27 +0100

    feat: enhance CLI config parser with error handling and improved parsing logic

--- includes/infrastructure/cli/cli_config_parser.h
+++ includes/infrastructure/cli/cli_config_parser.h
@@ -6,19 +6,30 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 19:54:04 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/06 11:30:15 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/07 16:59:25 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __CLI_CONFIG_PARSER_H__
-#define __CLI_CONFIG_PARSER_H__
+#ifndef CLI_CONFIG_PARSER_H
+# define CLI_CONFIG_PARSER_H
 
-#include <stdbool.h>
-#include "domain/config.h"
-#include "infrastructure/shared.h"
-#include "libft.h"
-#include <arpa/inet.h>
+# include "domain/config.h"
+# include "infrastructure/shared.h"
+# include "libft.h"
+# include <arpa/inet.h>
+# include <stdbool.h>
 
-bool parse_cli_config(t_config *config, int argc, char **argv);
+typedef enum e_conf_cli_parser
+{
+	CONFIG_CLI_PARSER_SUCCESS = 0,
+	CONFIG_CLI_INVALID_ARGS,
+	CONFIG_CLI_INVALID_SRC_IP,
+	CONFIG_CLI_INVALID_TGT_IP,
+	CONFIG_CLI_INVALID_SRC_MAC,
+	CONFIG_CLI_INVALID_TGT_MAC
+}					t_conf_cli_parser;
+
+t_conf_cli_parser	parse_cli_config(t_config *config, int argc, char **argv);
+char				*parse_cli_config_strerror(t_conf_cli_parser err);
 
 #endif
--- srcs/infrastructure/cli/cli_config_parser.c
+++ srcs/infrastructure/cli/cli_config_parser.c
@@ -6,25 +6,48 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 19:25:31 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/06 21:20:06 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/07 17:12:22 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "infrastructure/cli/cli_config_parser.h"
 #include "infrastructure/net/mac_parser.h"
 
-bool parse_cli_config(t_config *config, int argc, char **argv)
+char	*parse_cli_config_strerror(t_conf_cli_parser err)
 {
-    ft_memset(config, 0, sizeof(t_config));
-    if (argc != 5)
-        return ft_error("invalid arguments.\nUsage: ./ft_malcolm <src_ip> <src_mac> <tgt_ip> <tgt_mac>", EXIT_FAILURE);
-    if (inet_pton(AF_INET, argv[1], &config->src_ip) != 1)
-        return ft_error("invalid source ip.", false);
-    if (inet_pton(AF_INET, argv[3], &config->tgt_ip) != 1)
-        return ft_error("invalid target ip.", false);
-    if (!parse_str_mac_address(argv[2], &config->src_mac))
-        return false;
-    if (!parse_str_mac_address(argv[4], &config->tgt_mac))
-        return false;
-    return (true);
+	if (err == CONFIG_CLI_PARSER_SUCCESS)
+		return ("configuration ok");
+	else if (err == CONFIG_CLI_INVALID_ARGS)
+		return ("invalid number of arguments");
+	else if (err == CONFIG_CLI_INVALID_SRC_IP)
+		return ("invalid source ip address");
+	else if (err == CONFIG_CLI_INVALID_TGT_IP)
+		return ("invalid target ip address");
+	else if (err == CONFIG_CLI_INVALID_SRC_MAC)
+		return ("invalid source mac address");
+	else if (err == CONFIG_CLI_INVALID_TGT_MAC)
+		return ("invalid target mac address");
+	else
+		return ("unknown configuration error");
+}
+
+/// @brief Parses CLI arguments into a config struct
+/// @param config Pointer to the config struct to populate
+/// @param argc Number of CLI arguments
+/// @param argv Array of CLI argument strings
+/// @return
+t_conf_cli_parser	parse_cli_config(t_config *config, int argc, char **argv)
+{
+	ft_memset(config, 0, sizeof(t_config));
+	if (argc != 5)
+		return (CONFIG_CLI_INVALID_ARGS);
+	else if (inet_pton(AF_INET, argv[1], &config->src_ip) != 1)
+		return (CONFIG_CLI_INVALID_SRC_IP);
+	else if (inet_pton(AF_INET, argv[3], &config->tgt_ip) != 1)
+		return (CONFIG_CLI_INVALID_TGT_IP);
+	else if (!parse_str_mac_address(argv[2], &config->src_mac))
+		return (CONFIG_CLI_INVALID_SRC_MAC);
+	else if (!parse_str_mac_address(argv[4], &config->tgt_mac))
+		return (CONFIG_CLI_INVALID_TGT_MAC);
+	return (CONFIG_CLI_PARSER_SUCCESS);
 }
