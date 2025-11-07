aa6e507e13ee0c37e8e1a2f57af54a2dad216a48
DylanC-Code <dylanc.code@gmail.com>
2025-11-07 17:18:09 +0100

    style: improve formatting and consistency in hex_utils.h and hex_utils.c

--- includes/shared/hex_utils.h
+++ includes/shared/hex_utils.h
@@ -6,17 +6,17 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 12:41:51 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/06 13:12:43 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/07 17:17:30 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __HEX_UTILS_H__
-#define __HEX_UTILS_H__
+#ifndef HEX_UTILS_H
+# define HEX_UTILS_H
 
-#include <stdint.h>
-#include "libft.h"
+# include "libft.h"
+# include <stdint.h>
 
-void u8_to_hex_pair(uint8_t src, char dst[2]);
-uint8_t hex_pair_to_u8(char str[2]);
+void	u8_to_hex_pair(uint8_t src, char dst[2]);
+uint8_t	hex_pair_to_u8(char str[2]);
 
 #endif
--- srcs/shared/hex_utils.c
+++ srcs/shared/hex_utils.c
@@ -6,41 +6,47 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 12:41:37 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/06 13:43:54 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/07 17:16:40 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shared/hex_utils.h"
 
-uint8_t hex_pair_to_u8(char str[2])
+uint8_t	hex_pair_to_u8(char str[2])
 {
-    char high = ft_tolower(str[0]);
-    char low = ft_tolower(str[1]);
+	char	high;
+	char	low;
+	uint8_t	res;
 
-    uint8_t res = 0;
-    if (ft_isdigit(high))
-        res += ((high - '0') << 4);
-    else
-        res += ((high - 'a' + 10) << 4);
-    if (ft_isdigit(low))
-        res += (low - '0');
-    else
-        res += (low - 'a' + 10);
-    return res;
+	high = ft_tolower(str[0]);
+	low = ft_tolower(str[1]);
+	res = 0;
+	if (ft_isdigit(high))
+		res += ((high - '0') << 4);
+	else
+		res += ((high - 'a' + 10) << 4);
+	if (ft_isdigit(low))
+		res += (low - '0');
+	else
+		res += (low - 'a' + 10);
+	return (res);
 }
 
-char value_to_hex_char(uint8_t nibble)
+char	value_to_hex_char(uint8_t nibble)
 {
-    if (nibble < 10)
-        return nibble + '0';
-    else
-        return (nibble - 10) + 'a';
+	if (nibble < 10)
+		return (nibble + '0');
+	else
+		return ((nibble - 10) + 'a');
 }
 
-void u8_to_hex_pair(uint8_t src, char dst[2])
+void	u8_to_hex_pair(uint8_t src, char dst[2])
 {
-    int low = src & 0x0F;
-    int high = (src >> 4) & 0x0F;
-    dst[0] = value_to_hex_char(high);
-    dst[1] = value_to_hex_char(low);
+	int	low;
+	int	high;
+
+	low = src & 0x0F;
+	high = (src >> 4) & 0x0F;
+	dst[0] = value_to_hex_char(high);
+	dst[1] = value_to_hex_char(low);
 }
