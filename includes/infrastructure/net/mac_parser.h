a0eee96272b1fd7b464da492609d52caaf3da35d
DylanC-Code <dylanc.code@gmail.com>
2025-11-07 17:19:03 +0100

    refactor: improve formatting and consistency in MAC parser header and implementation

--- includes/infrastructure/net/mac_parser.h
+++ includes/infrastructure/net/mac_parser.h
@@ -6,25 +6,25 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 11:57:43 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/06 14:03:27 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/07 17:10:55 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __MAC_PARSER_H__
-#define __MAC_PARSER_H__
+#ifndef MAC_PARSER_H
+# define MAC_PARSER_H
 
-#include <stdint.h>
-#include <stdbool.h>
-#include "domain/mac.h"
+# include "domain/mac.h"
+# include <stdbool.h>
+# include <stdint.h>
 
 typedef enum e_mac_parse_error
 {
-    MAC_PARSING_SUCCESS = 0,
-    INVALID_MAC_LENGTH = 17,
-    INVALID_MAC_FORMAT = 18,
-    INVALID_MAC_CHAR = 19,
-} t_mac_parse_error;
+	MAC_PARSING_SUCCESS = 0,
+	INVALID_MAC_LENGTH = 17,
+	INVALID_MAC_FORMAT = 18,
+	INVALID_MAC_CHAR = 19,
+}		t_mac_parse_error;
 
-bool parse_str_mac_address(char *src, t_mac *dst);
+bool	parse_str_mac_address(char *src, t_mac *dst);
 
 #endif
--- srcs/infrastructure/net/mac_parser.c
+++ srcs/infrastructure/net/mac_parser.c
@@ -6,65 +6,73 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 11:58:03 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/06 13:59:34 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/07 17:08:51 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "infrastructure/net/mac_parser.h"
 #include "infrastructure/shared.h"
-#include "shared/hex_utils.h"
 #include "libft.h"
+#include "shared/hex_utils.h"
 
-static bool is_valid_char(char c)
+static bool	is_valid_char(char c)
 {
-    return ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || c == '*');
+	return ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || c == '*');
 }
 
-static bool is_sep_pos(size_t i)
+static bool	is_sep_pos(size_t i)
 {
-    return ((i + 1) % 3 == 0);
+	return ((i + 1) % 3 == 0);
 }
 
-static t_mac_parse_error valid_str_mac_address(char *mac)
+static t_mac_parse_error	valid_str_mac_address(char *mac)
 {
-    size_t i = -1;
-    if (!mac)
-        return INVALID_MAC_FORMAT;
-    while (mac[++i])
-    {
-        if (i >= 17)
-            return INVALID_MAC_LENGTH;
-        if (!is_sep_pos(i) && !is_valid_char(mac[i]))
-            return INVALID_MAC_CHAR;
-        if (is_sep_pos(i) && mac[i] != ':')
-            return INVALID_MAC_FORMAT;
-    }
-    if (i != 17)
-        return INVALID_MAC_LENGTH;
-    return MAC_PARSING_SUCCESS;
+	size_t	i;
+
+	i = -1;
+	if (!mac)
+		return (INVALID_MAC_FORMAT);
+	while (mac[++i])
+	{
+		if (i >= 17)
+			return (INVALID_MAC_LENGTH);
+		if (!is_sep_pos(i) && !is_valid_char(mac[i]))
+			return (INVALID_MAC_CHAR);
+		if (is_sep_pos(i) && mac[i] != ':')
+			return (INVALID_MAC_FORMAT);
+	}
+	if (i != 17)
+		return (INVALID_MAC_LENGTH);
+	return (MAC_PARSING_SUCCESS);
 }
 
-static char *mac_parse_strerror(t_mac_parse_error err)
+static char	*mac_parse_strerror(t_mac_parse_error err)
 {
-    if (err == MAC_PARSING_SUCCESS)
-        return "mac address ok";
-    else if (err == INVALID_MAC_LENGTH)
-        return "mac address invalid length (must be 17)";
-    else if (err == INVALID_MAC_FORMAT)
-        return "mac address invalid format (use xx:xx:xx:xx:xx:xx)";
-    else if (err == INVALID_MAC_CHAR)
-        return "mac address invalid character";
-    else
-        return "mac parsing unknown error";
+	if (err == MAC_PARSING_SUCCESS)
+		return ("mac address ok");
+	else if (err == INVALID_MAC_LENGTH)
+		return ("mac address invalid length (must be 17)");
+	else if (err == INVALID_MAC_FORMAT)
+		return ("mac address invalid format (use xx:xx:xx:xx:xx:xx)");
+	else if (err == INVALID_MAC_CHAR)
+		return ("mac address invalid character");
+	else
+		return ("mac parsing unknown error");
 }
 
-bool parse_str_mac_address(char *src, t_mac *dst)
+bool	parse_str_mac_address(char *src, t_mac *dst)
 {
-    t_mac_parse_error parse_res = valid_str_mac_address(src);
-    if (parse_res != MAC_PARSING_SUCCESS)
-        return ft_error(mac_parse_strerror(parse_res), false);
-    int i = -1;
-    while (++i < 6)
-        dst->bytes[i] = hex_pair_to_u8(src + i + i * 2);
-    return true;
+	t_mac_parse_error	parse_res;
+	int					i;
+
+	parse_res = valid_str_mac_address(src);
+	if (parse_res != MAC_PARSING_SUCCESS)
+	{
+		ft_error(mac_parse_strerror(parse_res));
+		return (false);
+	}
+	i = -1;
+	while (++i < 6)
+		dst->bytes[i] = hex_pair_to_u8(src + i + i * 2);
+	return (true);
 }
