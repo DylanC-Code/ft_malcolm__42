fed578e867d39efe19b719142678b810699c4657
DylanC-Code <dylanc.code@gmail.com>
2025-11-07 17:19:22 +0100

    refactor: update exit codes for consistency and improve error handling in main function

--- includes/ft_malcolm.h
+++ includes/ft_malcolm.h
@@ -6,33 +6,29 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 18:42:05 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/06 21:38:29 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/07 17:11:57 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_MALCOM_H
-#define FT_MALCOM_H
+#ifndef FT_MALCOLM_H
+# define FT_MALCOLM_H
+
+# include "core/arp_service.h"
+# include "domain/config.h"
+# include "domain/mac.h"
+# include "infrastructure/cli/cli_config_parser.h"
+# include "infrastructure/cli/cli_usage.h"
+# include "infrastructure/net/mac_parser.h"
+# include "infrastructure/permissions.h"
+# include "infrastructure/shared.h"
 
 typedef enum e_exit_code
 {
-    SUCCESS_CODE,
-    NOT_ROOT_ERR_CODE = 66,
-    PARSING_CONFIG_ERR_CODE,
-    INVALID_CONFIG_ERR_CODE,
-    SPOOFING_ERR_CODE,
-
-} t_exit_code;
-
-// #include <stdlib.h>
-
-#include "domain/mac.h"
-#include "domain/config.h"
-
-#include "infrastructure/shared.h"
-#include "infrastructure/permissions.h"
-#include "infrastructure/cli/cli_config_parser.h"
-#include "infrastructure/net/mac_parser.h"
-
-#include "core/arp_service.h"
+	SUCCESS_EXIT_CODE,
+	NOT_ROOT_EXIT_CODE = 66,
+	PARSING_CONFIG_EXIT_CODE,
+	INVALID_CONFIG_EXIT_CODE,
+	SPOOFING_EXIT_CODE,
+}	t_exit_code;
 
 #endif
--- srcs/ft_malcolm.c
+++ srcs/ft_malcolm.c
@@ -6,23 +6,29 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 18:42:08 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/06 21:38:31 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/07 17:12:03 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_malcolm.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_config config;
+	t_config			config;
+	t_conf_cli_parser	parse_status;
 
-    if (!checking_perm())
-        return NOT_ROOT_ERR_CODE;
-    if (!parse_cli_config(&config, argc, argv))
-        return PARSING_CONFIG_ERR_CODE;
-    // if (!checking_config(&config)) //TODO: verify not same mac and ip and maybe other things
-    //     return INVALID_CONFIG_ERR_CODE;
-    if (!listen_arp_request_and_reply(&config))
-        return SPOOFING_ERR_CODE;
-    return SUCCESS_CODE;
+	if (!checking_perm())
+		return (NOT_ROOT_EXIT_CODE);
+	parse_status = parse_cli_config(&config, argc, argv);
+	if (parse_status != CONFIG_CLI_PARSER_SUCCESS)
+	{
+		print_cli_error_and_usage(parse_status);
+		return (PARSING_CONFIG_EXIT_CODE);
+	}
+	// if (!checking_config(&config))
+	// TODO: verify not same mac and ip and maybe other things
+	//     return (INVALID_CONFIG_ERR_CODE);
+	if (!listen_arp_request_and_reply(&config))
+		return (SPOOFING_EXIT_CODE);
+	return (SUCCESS_EXIT_CODE);
 }
