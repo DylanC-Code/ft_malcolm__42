902b171098a0b1a6662470dd0877d97dde617c4c
DylanC-Code <dylanc.code@gmail.com>
2025-11-07 17:18:17 +0100

    refactor: update arp_service.h and arp_service.c for improved readability and structure

--- includes/core/arp_service.h
+++ includes/core/arp_service.h
@@ -6,18 +6,17 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 21:29:59 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/06 21:56:04 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/07 17:16:52 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __ARP_SERVICE_H__
-#define __ARP_SERVICE_H__
+#ifndef ARP_SERVICE_H
+# define ARP_SERVICE_H
 
-#include <stdbool.h>
+# include "domain/config.h"
+# include "infrastructure/iface.h"
+# include <stdbool.h>
 
-#include "domain/config.h"
-#include "infrastructure/iface.h"
-
-bool listen_arp_request_and_reply(t_config *config);
+bool	listen_arp_request_and_reply(t_config *config);
 
 #endif
--- srcs/core/arp_service.c
+++ srcs/core/arp_service.c
@@ -6,31 +6,43 @@
 /*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 21:29:46 by dylan             #+#    #+#             */
-/*   Updated: 2025/11/07 12:30:01 by dylan            ###   ########.fr       */
+/*   Updated: 2025/11/07 17:12:13 by dylan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "core/arp_service.h"
+#include "domain/arp/arp_parser.h"
 #include "infrastructure/shared.h"
 #include <unistd.h>
 
-bool listen_arp_request_and_reply(t_config *config)
+/// @brief Listens for ARP requests and replies accordingly
+/// @param config Pointer to the configuration struct
+/// @return true on success, false on failure
+bool	listen_arp_request_and_reply(t_config *config)
 {
-    t_iface *iface;
-    t_iface_status iface_status = iface_open(&iface);
-    unsigned char buffer[65536];
-    ssize_t bytes_read = 0;
+	t_iface			*iface;
+	t_iface_status	iface_status;
+	unsigned char	buffer[65536];
+	ssize_t			bytes_read;
+	t_arp_packet	arp_packet;
 
-    if (iface_status != IFACE_OPENING_SUCCESS)
-        return (ft_error(iface_strerror(iface_status), false));
-    while (!config->once)
-    {
-        sleep(1);
-        iface_status = iface_recv(iface, buffer, sizeof(buffer), &bytes_read);
-        if (iface_status != IFACE_RECV_SUCCESS)
-            ft_error(iface_strerror(iface_status), 0);
-    }
-    iface_close(iface);
-    __builtin_printf("iface close correctly\n");
-    return true;
+	iface_status = iface_open(&iface);
+	bytes_read = 0;
+	if (iface_status != IFACE_OPENING_SUCCESS)
+		return (ft_error(iface_strerror(iface_status)), false);
+	while (!config->once)
+	{
+		sleep(1);
+		iface_status = iface_recv(iface, buffer, sizeof(buffer), &bytes_read);
+		if (iface_status != IFACE_RECV_SUCCESS)
+		{
+			ft_error(iface_strerror(iface_status));
+			/* TODO: handle which error will made quit the program. */
+			continue ;
+		}
+		parse_arp_request(buffer, bytes_read, &arp_packet);
+	}
+	iface_close(iface);
+	__builtin_printf("iface close correctly\n");
+	return (true);
 }
